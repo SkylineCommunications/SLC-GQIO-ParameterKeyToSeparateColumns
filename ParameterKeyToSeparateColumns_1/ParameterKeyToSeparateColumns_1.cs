91f5bd16d76ba22b84edd567af2d11c149bd3413
Pieter Van Compernolle <pieter.van.compernolle@skyline.be>
2024-06-07 19:34:14 +0200

    fix warnings

--- ParameterKeyToSeparateColumns_1/ParameterKeyToSeparateColumns_1.cs
+++ ParameterKeyToSeparateColumns_1/ParameterKeyToSeparateColumns_1.cs
@@ -1,57 +1,47 @@
 using System;
 using Skyline.DataMiner.Analytics.GenericInterface;
 using Skyline.DataMiner.Net;
-using Skyline.DataMiner.Net.Messages;
-using System.Diagnostics;
-using System.Text;
-using System.Collections.Generic;
-
 
 [GQIMetaData(Name = "ParameterKeyToSeparateColumns")]
 public class ParameterKeyToSeparateColumns : IGQIRowOperator, IGQIInputArguments, IGQIColumnOperator
 {
-    private readonly GQIColumnDropdownArgument _ParameterKeyColumnArg = new GQIColumnDropdownArgument("Parameter Key Column") { IsRequired = true };
+    private readonly GQIColumnDropdownArgument _parameterKeyColumnArg = new GQIColumnDropdownArgument("Parameter Key Column") { IsRequired = true };
+    private readonly GQIColumn _dMAIDColumn = new GQIIntColumn("DMA ID (int)");
+    private readonly GQIColumn _elementIDColumn = new GQIIntColumn("Element ID (int)");
+    private readonly GQIColumn _parameterIDColumn = new GQIIntColumn("Parameter ID (int)");
+    private readonly GQIColumn _tableIndexColumn = new GQIStringColumn("Table Index (String)");
+
     private GQIColumn _parameterKeyColumn;
-
-    private readonly GQIColumn _DMAIDColumn = new GQIIntColumn("DMA ID (int)");
-    private readonly GQIColumn _ElementIDColumn = new GQIIntColumn("Element ID (int)");
-    private readonly GQIColumn _ParameterIDColumn = new GQIIntColumn("Parameter ID (int)");
-    private readonly GQIColumn _TableIndexColumn = new GQIStringColumn("Table Index (String)");
-
 
     public GQIArgument[] GetInputArguments()
     {
-        return new GQIArgument[] { _ParameterKeyColumnArg };
+        return new GQIArgument[] { _parameterKeyColumnArg };
     }
 
     public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
     {
-        _parameterKeyColumn = args.GetArgumentValue(_ParameterKeyColumnArg);
+        _parameterKeyColumn = args.GetArgumentValue(_parameterKeyColumnArg);
 
         return new OnArgumentsProcessedOutputArgs();
     }
 
     public void HandleColumns(GQIEditableHeader header)
     {
-        header.AddColumns(_DMAIDColumn);
-        header.AddColumns(_ElementIDColumn);
-        header.AddColumns(_ParameterIDColumn);
-        header.AddColumns(_TableIndexColumn);
+        header.AddColumns(_dMAIDColumn);
+        header.AddColumns(_elementIDColumn);
+        header.AddColumns(_parameterIDColumn);
+        header.AddColumns(_tableIndexColumn);
     }
 
     public void HandleRow(GQIEditableRow row)
     {
-        try
-        {
-            // Convert via ParamID Class
-            String parameterKey = row.GetValue<String>(_parameterKeyColumn);
-            ParamID paramID = ParamID.FromString(parameterKey);
+        // Convert via ParamID Class
+        String parameterKey = row.GetValue<String>(_parameterKeyColumn);
+        ParamID paramID = ParamID.FromString(parameterKey);
 
-            row.SetValue(_DMAIDColumn, paramID.DataMinerID);
-            row.SetValue(_ElementIDColumn, paramID.EID);
-            row.SetValue(_ParameterIDColumn, paramID.PID);
-            row.SetValue(_TableIndexColumn, paramID.TableIdx);
-        }
-        catch (Exception ex) { }
+        row.SetValue(_dMAIDColumn, paramID.DataMinerID);
+        row.SetValue(_elementIDColumn, paramID.EID);
+        row.SetValue(_parameterIDColumn, paramID.PID);
+        row.SetValue(_tableIndexColumn, paramID.TableIdx);
     }
 }
